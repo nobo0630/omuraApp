6009f85daa004e2ee9c19f9d0c46720a75dbe2ac
nobo0630 <fantasista_no_22@yahoo.co.jp>
2018-11-10 16:28:34 +0900

    Initial Commit

---
+++ SoccerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoccerApp
+//
+//  Created by 大村昇 on 2018/11/10.
+//  Copyright © 2018 大村昇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
